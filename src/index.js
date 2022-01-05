f27499adb46cc114debfb5c672b449415ed38498
kurapoko <kuraharamasahiro@gmail.com>
2022-01-06 05:42:47 +0900

    init

--- src/index.js
+++ src/index.js
@@ -1,10 +1,68 @@
-import "./styles.css";
+import MicroModal from "micromodal";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// var player;
+// var is_yt_ready = false;
+// let do_play = false;
+// const w = 900;
+// const h = 600;
+// const tag = document.createElement("script");
+// tag.src = "https://www.youtube.com/iframe_api";
+// var firstScriptTag = document.getElementsByTagName("script")[0];
+// var onPlayerReady = function (e) {
+//   is_yt_ready = true;
+// };
+// const onPlayerStateChange = function (e) {
+//   if (e.data == YT.PlayerState.ENDED) {
+//   } else if (e.data == YT.PlayerState.PLAYING) {
+//   } else if (e.data == YT.PlayerState.PAUSED) {
+//   } else if (e.data == YT.PlayerState.BUFFERING) {
+//   } else if (e.data == YT.PlayerState.CUED) {
+//     if (do_play) {
+//       do_play = false;
+//       document.querySelector(".c-modal-movie-content").style.visibility =
+//         "visible";
+//       e.target.playVideo();
+//     }
+//   } else {
+//   }
+// };
+
+// onPlayerReady();
+// firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+// window.onYouTubeIframeAPIReady = function () {
+//   player = new YT.Player("modal_yt_player", {
+//     width: w,
+//     height: h,
+//     playerVars: { autoplay: 1 },
+//     videoId: "M7lc1UVf-VE",
+//     events: {
+//       onReady: onPlayerReady,
+//       onStateChange: onPlayerStateChange
+//     }
+//   });
+// };
+
+// MicroModal.show("modal-movie", {
+//   onShow: function (modal, trig, eve) {
+//     document.querySelector(".c-modal-movie-content").style.visibility =
+//       "hidden";
+
+//     if (!is_yt_ready) {
+//       MicroModal.close("modal-movie");
+//       return;
+//     }
+//     do_play = true;
+//   }
+//   onClose: function (modal, trig, eve) {
+//     if (is_yt_ready) {
+//       player.stopVideo();
+//       eve.stopPropagation();
+//     }
+//   }
+// });
+
+setTimeout(function () {
+  MicroModal.show("modal-movie", {
+    onShow: function (modal) {}
+  });
+}, 5000);
